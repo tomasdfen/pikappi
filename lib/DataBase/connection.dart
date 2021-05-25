8bdce1d14c5968cf11e3ebbd2823146743228a31
tomasdfen <tomasdfen@gmail.com>
2021-05-25 20:41:56 +0200

    añadido pokemon favorito y pokedex dinamica

--- lib/DataBase/connection.dart
+++ lib/DataBase/connection.dart
@@ -18,13 +18,13 @@ Future<Map<String, dynamic>> getUser() async {
     map['gender'] = v['gender'];
     map['birthday'] = v['birthday'];
     map['fav_pokemon'] = v['fav_pokemon'];
+    map['captured'] = v['captured'];
   });
   await db.close();
   return map;
 }
 
 Future<String> getUserName() async {
-  print("Ubuntu");
   var db = await Db.create(
       "mongodb+srv://admin:tfgadmin@cluster0.cjbui.mongodb.net/pikappi");
   await db.open();
@@ -46,7 +46,6 @@ Future<String> getUserName() async {
 }
 
 Future<String> updateUserGender(String gender) async {
-  print("Ubuntu");
   var db = await Db.create(
       "mongodb+srv://admin:tfgadmin@cluster0.cjbui.mongodb.net/pikappi");
   await db.open();
@@ -54,8 +53,38 @@ Future<String> updateUserGender(String gender) async {
   print('>> Updating User');
   var collection = db.collection('user');
 
-  collection.updateOne(where.eq('_id', 'usuario'), modify.set('gender', gender));
+  collection.updateOne(
+      where.eq('_id', 'usuario'), modify.set('gender', gender));
 
   await db.close();
   return "Changed gender to " + gender;
+}
+
+  updateFavPokemon(int fav) async {
+  var db = await Db.create(
+      "mongodb+srv://admin:tfgadmin@cluster0.cjbui.mongodb.net/pikappi");
+  await db.open();
+  print('====================================================================');
+  print('>> Updating User');
+  var collection = db.collection('user');
+
+  collection.updateOne(
+      where.eq('_id', 'usuario'), modify.set('fav_pokemon', fav));
+
+  await db.close();
+
+}
+
+Future<List<dynamic>> getPokemonList() async {
+  var db = await Db.create(
+      "mongodb+srv://admin:tfgadmin@cluster0.cjbui.mongodb.net/pikappi");
+  await db.open();
+  print('====================================================================');
+  print('>> Updating User');
+  var collection = db.collection('user');
+  var temp =
+      await collection.findOne(where.eq("_id", 'usuario').fields(['captured']));
+  var captured = temp['captured'];
+  await db.close();
+  return captured;
 }
