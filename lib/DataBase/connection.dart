79272f74b9c1991dd3941d85f26da3ec2a94053d
tomasdfen <tomasdfen@gmail.com>
2021-05-26 16:26:37 +0200

    connection con getFavPokemon

--- lib/DataBase/connection.dart
+++ lib/DataBase/connection.dart
@@ -75,6 +75,27 @@ Future<String> updateUserGender(String gender) async {
 
 }
 
+Future<int> getFavPokemon() async {
+  var db = await Db.create(
+      "mongodb+srv://admin:tfgadmin@cluster0.cjbui.mongodb.net/pikappi");
+  await db.open();
+  print('====================================================================');
+  print('>> Adding Authors');
+  var collection = db.collection('user');
+
+  Map<String, dynamic> map = {};
+  await collection.find().forEach((v) {
+    map['_id'] = v['_id'];
+    map['image'] = v['image'];
+    map['name'] = v['name'];
+    map['gender'] = v['gender'];
+    map['birthday'] = v['birthday'];
+    map['fav_pokemon'] = v['fav_pokemon'];
+  });
+  await db.close();
+  return map['fav_pokemon'];
+}
+
 Future<List<dynamic>> getPokemonList() async {
   var db = await Db.create(
       "mongodb+srv://admin:tfgadmin@cluster0.cjbui.mongodb.net/pikappi");
